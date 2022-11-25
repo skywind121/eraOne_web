73f478a86defc6a64a55ff8df16a8f84f357777d
skywind121 <46217270+skywind121@users.noreply.github.com>
2022-11-25 23:05:10 +0800

    ver 0.1
    test version

---
+++ component/header.js
@@ -0,0 +1,26 @@
+header ='<div class="navbar navbar-default navbar-static-top">\
+        <div class="container">\
+            <div class="navbar-header">\
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                </button>\
+                <a href="index.html"><img src="img/logo.png" alt="logo"/></a>\
+            </div>\
+            <div class="navbar-collapse collapse ">\
+                <ul class="nav navbar-nav">\
+                    <li class="active"><a href="index.html">首頁</a></li> \
+                    <li><a href="about.html">關於泰殿</a></li>\
+                    <li><a href="courses.html">Courses</a></li>\
+                    <li><a href="portfolio.html">Portfolio</a></li>\
+                    <li><a href="pricing.html">Pricing</a></li>\
+                    <li><a href="contact.html">Contact</a></li>\
+                </ul>\
+            </div>\
+        </div>\
+        </div>';
+document.write(header);
+
+
+        
